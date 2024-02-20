9871949dd13817a0cf93896b2adbe8dd052bd540
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2024-02-20 15:45:33 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,5 @@
+//exercise 10
+//my name is Momin
+//dated: 20/02/2024
+//this program will simple code just like hello wworld
+console.log('hello world simple program');
---
+++ program2.ts
@@ -0,0 +1,5 @@
+//exercise 10
+//my name is Momin
+//dated: 20/02/2024
+//this program will adding
+console.log(9 +5);
